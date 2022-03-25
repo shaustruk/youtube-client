f22dd8079df0e13bf1faca7352016720a91e537f
Your Name <you@example.com>
2022-03-25 11:59:19 +0300

    create interfaces for response.json

--- my-app/src/app/main-page/main/main.component.ts
+++ my-app/src/app/main-page/main/main.component.ts
@@ -1,5 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IIMg {
+  url: string,
+  width: number,
+  height: number
+}
+
+export interface IThumbnails {
+  default: IIMg,
+  medium: IIMg,
+  high: IIMg,
+  standard: IIMg,
+  maxres: IIMg
+}
+export interface Ilocalized {
+  title: string,
+  description: string
+}
+
+export interface ISnippet {
+  publishedAt: string,
+  channelId: string,
+  title: string,
+  description: string,
+  thumbnails: IThumbnails,
+  channelTitle: string,
+  tags: [index: string],
+  categoryId: number,
+  liveBroadcastContent: string,
+  localized: Ilocalized
+  defaultAudioLanguage: string
+}
+
+export interface IStatistics {
+  viewCount: string,
+  likeCount: string,
+  dislikeCount: string,
+  favoriteCount: string,
+  commentCount: string
+}
+
+export interface IItem {
+  kind: string,
+  etag: string,
+  id: string,
+  snippet: ISnippet,
+  statisticks: IStatistics,
+
+
+
+}
+export interface IItems {
+  kind: string,
+  etag: string,
+  pageInfo: {
+    totalResults: number,
+    resultsPerPage: number
+    card: IItem[];
+
+  }
+}
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
