16dc32feb31287b963b8e8ed0f14c28699fbb420
Your Name <you@example.com>
2022-03-31 01:44:19 +0300

    feat: connect angular-material

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header-components/header/header.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { SearchAreaComponent } from './header-components/search-area/search-area.component';
 import { UserInfoComponent } from './header-components/user-info/user-info.component';
-import { SortsSearchAreaComponent } from './header-components/sorts-search-area/sorts-search-area.component';
+import { SortsSearchAreaComponent } from './sorts-search-area/sorts-search-area.component';
 import { SearchResultModelComponent } from './header-components/search-result-model/search-result-model.component';
 import { SearchItemModelComponent } from './header-components/search-item-model/search-item-model.component';
 import { SearchItemComponent } from './header-components/search-item/search-item.component';
 import { SearchResultsComponent } from './header-components/search-results/search-results.component';
+import { SettingsButtonComponent } from './header-components/settings-button/settings-button.component';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -25,10 +28,13 @@ import { SearchResultsComponent } from './header-components/search-results/searc
     SearchItemModelComponent,
     SearchItemComponent,
     SearchResultsComponent,
+    SettingsButtonComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
